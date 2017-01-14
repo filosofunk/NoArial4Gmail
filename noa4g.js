7c35de0d3ff9e682df60773b5478290cf7307d05
Michael <github@filosofunk.com>
2017-01-14 11:46:17 -0600

    Update noa4g.js

--- noa4g.js
+++ noa4g.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Gmail font change
 // @namespace    http://filosofunk.com/
-// @version      0.1
-// @description  Changes the diaplay font in Gmail to Verdana, sans-derif
+// @version      0.2
+// @description  Changes the diaplay font in Gmail to Roboto, Verdana, sans-serif
 // @author       Michael Funk
 // @match        https://mail.google.com/* 
 // @grant        none
@@ -15,4 +15,5 @@ var sheet = (function() {
 	return style.sheet;
 })();
 
-sheet.insertRule("body, td, input, select, textarea { font-family : Verdana,sans-serif !important; }", 0);
+sheet.insertRule("body, td, select, textarea { font-family : Roboto,Verdana,sans-serif !important; }", 0);
+sheet.insertRule("input[name='subjectbox'] { font-family : Roboto,Verdana,sans-serif !important; font-weight: bold; }", 0);
